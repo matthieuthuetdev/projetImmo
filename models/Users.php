75e90b7c443fd0a1836bf3bfbe1e266c42020190
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-10-22 12:55:09 +0200

    restructuration des fichier

--- models/Users.php
+++ models/Users.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 class Users
 {
     private PDO $connection;
@@ -8,12 +9,19 @@ class Users
     }
     public function signIn($_userName, $_password)
     {
-        $request = "SELECT * FROM utilisateurs WHERE nom_utilisateur= :username AND pass_utilisateur= :password";
+        $request = "SELECT * FROM utilisateurs WHERE mail_utilisateur= :username";
         $rq = $this->connection->prepare($request);
         $rq->bindParam(":username", $_userName, PDO::PARAM_STR);
-        $rq->bindParam(":password", $_password, PDO::PARAM_STR);
-        if ($rq->execute()) {
-            return true;
+        $rq->execute();
+        $result = $rq->fetchAll(PDO::FETCH_ASSOC);
+        if (count($result) == 1) {
+            $succes = password_verify($_password, $result[0]["pass_utilisateur"]);
+            if($succes){
+                $_SESSION["mail"] = $result[0]["mail_utilisateur"];
+                $_SESSION["id"] = $result[0]["id_utilisateur"];
+    
+            }
+            return $succes
         } else {
             return false;
         }
